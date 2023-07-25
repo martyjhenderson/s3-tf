f0f9739c8311f65bf059df4c1922bc3ff0953190
Marty Henderson <marty@ravegrunt.is>
2023-07-25 12:38:07 +0000

    Updating version to 1.5

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.4"
+  required_version = ">= 1.5"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.0.1"
+      version = "5.9.0"
     }
     random = {
       source  = "hashicorp/random"
